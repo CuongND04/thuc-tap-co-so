646a9cad2b528b499b061937f49ca545862ae6f2
Nguyễn Đức Cường <uteu237@gmail.com>
2025-05-17 09:37:18 +0700

    add JWT authentication, token for login, register

---
+++ backend/src/main/java/com/pet/shop/dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.pet.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class ErrorDto {
+
+    private String message;
+}
