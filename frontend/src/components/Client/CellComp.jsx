83d642220c425d5a7cbcbf2afcda9ed72f5caa7d
Nguyễn Đức Duy <fishsyp21@gmail.com>
2025-05-28 22:51:03 +0700

    fix home page components

--- frontend/src/components/Client/CellComp.jsx
+++ frontend/src/components/Client/CellComp.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const CellComp = ({prodID, imgSource, prodName, price, newStat}) => {
+const CellComp = ({prodID, imgSource, prodName, price, rating, newStat}) => {
   newStat = newStat || false;
   return (
-    <div className="article-iteam">
-                  <div className="product-iteam">
-                    <div className="product-thumb">
-                      <div className="product-thumb-img">
+    <div className="flex w-[25%] justify-center items-center mt-[10px] mb-[10px] ml-0 mr-0">
+                  <div className="group/a1">
+                    <div className="">
+                      <div className="relative overflow-hidden group">
                         <a href={`/san-pham/${prodID}`}>
-                          {newStat && <div className="badge">
-                            <p>New</p>
+                          {newStat && <div className="absolute top-[10px] right-[10px] h-[40px] w-[40px] flex justify-center items-center bg-[#8bc34a] rounded-[50%] group-hover:hidden">
+                            <p className="w-[35px] h-[35px] flex justify-center items-center border-[1px] border-dotted border-white text-white font-[Dosis] rounded-[50%]">New</p>
                           </div>}
-                          <div className="img">
+                          <div className="group-hover:scale-110 group-hover:bg-white/50 transform transition duration-300 ease-in-out">
                             <img
                               src={imgSource}
                               alt="img"
@@ -20,106 +20,47 @@ const CellComp = ({prodID, imgSource, prodName, price, newStat}) => {
                             />
                           </div>
                         </a>
-                        <div className="function-icon">
+                        <div className="absolute hidden top-[35%] left-[27%] group-hover:block">
                           <a href="">
-                            <i className="fa-regular fa-heart">
-                              <div className="tooltip">Yêu Thích</div>
+                            <i className="mt-0 mb-0 mr-[10px] ml-[10px] bg-[#de8ebe] w-[40px] h-[40px] text-white text-[18px] inline-flex justify-center items-center rounded-[100px] fa-regular fa-heart hover:text-[#de8ebe] hover:bg-white group/b1">
+                              <div className="absolute bottom-[50px] left-[50%] bg-[#de8ebe] text-white text-[12px] font-[Arial] font-[200] whitespace-nowrap pt-[5px] pb-[5px] pr-[10px] pl-[10px] rounded-[5px] transform hidden -translate-x-1/2 z-100 group-hover/b1:block">Yêu Thích</div>
                             </i>
                           </a>
                           <a href={`/san-pham/${prodID}`}>
-                            <i className="fa-solid fa-magnifying-glass">
-                              <div className="tooltip">Xem Nhanh</div>
+                            <i className="mt-0 mb-0 mr-[10px] ml-[10px] bg-[#de8ebe] w-[40px] h-[40px] text-white text-[18px] inline-flex justify-center items-center rounded-[100px] hover:text-[#de8ebe] hover:bg-white group/b1 fa-solid fa-magnifying-glass">
+                              <div className="absolute bottom-[50px] left-[50%] bg-[#de8ebe] text-white text-[12px] font-[Arial] font-[200] whitespace-nowrap pt-[5px] pb-[5px] pr-[10px] pl-[10px] rounded-[5px] transform hidden -translate-x-1/2 z-100 group-hover/b1:block">Xem Nhanh</div>
                             </i>
                           </a>
                         </div>
-                        <div className="function-buy">
-                          <div className="function-buy-boder">
-                            <button onclick="buy(this)" id="useFunctionBtn">
-                              <span>Thêm vào giỏ</span>
+
+                        <div className="absolute hidden bottom-[90px] left-[50%] bg-[#de8ebe] rounded-[10px] transform -translate-x-[50%] group-hover:block hover:bg-[#cf72aa]">
+                          <div className="flex justify-center items-center w-[140px] h-[40px] rounded-[10px]">
+                            <button class="flex justify-center items-center bg-[#de8ebe] w-[130px] h-[35px] rounded-[10px] border-[1px] border-dashed border-[#fff] hover:bg-[#cf72aa]" id="useFunctionBtn">
+                              <span className="text-white text-[14px] font-bold after:content-['\276F']
+  after:m-[10px] after:font-[400]">Thêm vào giỏ</span>
                             </button>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div className="product-info">
-                      <div className="product-info-id">
-                        <span>ID:{prodID}</span>
-                        <div className="product-info-rate">
-                          <i className="fa-regular fa-star"></i>
-                          <i className="fa-regular fa-star"></i>
-                          <i className="fa-regular fa-star"></i>
-                          <i className="fa-regular fa-star"></i>
-                          <i className="fa-regular fa-star"></i>
+                    <div className="">
+                      <div className="relative mt-[15px] mb-[15px] ml-0 mr-0 flex justify-between">
+                        <span className="font-bold text-[#de8ebe] text-[12px]">ID:{prodID}</span>
+                        <div className="absolute hidden right-0 group-hover/a1:block">
+                         {rating>=1 ? (<i className="fa-regular fa-star text-amber-500"></i>) : (<i className="fa-regular fa-star text-gray-500"></i>)}
+                          {rating>=2 ? (<i className="fa-regular fa-star text-amber-500"></i>) : (<i className="fa-regular fa-star text-gray-500"></i>)}
+                          {rating>=3 ? (<i className="fa-regular fa-star text-amber-500"></i>) : (<i className="fa-regular fa-star text-gray-500"></i>)}
+                          {rating>=4 ? (<i className="fa-regular fa-star text-amber-500"></i>) : (<i className="fa-regular fa-star text-gray-500"></i>)}
+                          {rating>=5 ? (<i className="fa-regular fa-star text-amber-500"></i>) : (<i className="fa-regular fa-star text-gray-500"></i>)}
                         </div>
                       </div>
-                      <h3>
+                      <h3 className="font-[Coiny] overflow-hidden whitespace-nowrap text-ellipsis max-w-[250px] hover:text-[#cf72aa]">
                         <a href={`/san-pham/${prodID}`}>{prodName}</a>
                       </h3>
-                      <p>{price}₫</p>
+                      <p className="font-[Arial] text-[18px] pt-0 pb-0 pr-[2px] pl-[2px] mt-[20px] mb-[20px] mr-0 ml-0 font-extrabold text-[#cf72aa]">{price}₫</p>
                     </div>
                   </div>
                 </div>
-
-    // <div className="article-iteam">
-    //             <div className="product-iteam">
-    //               <div className="product-thumb">
-    //                 <div className="relative overflow-hidden group">
-    //                   <a href="#">   
-    //                     <div className="absolute top-[10px] right-[10px] h-[40px] w-[40px] flex justify-center items-center bg-[#8bc34a] rounded-[20px] group-hover:hidden">
-    //                         <p className="w-[35px] h-[35px] flex justify-center items-center border-[1px] border-dotted border-white text-white font-[Dosis] rounded-[17.5px]">New</p>
-    //                     </div>
-
-    //                     <div className="group-hover:scale-110 group-hover:bg-white/50 transform transition duration-300 ease-in-out">
-    //                       <img
-    //                         src={imgSource} ///IMG/TrangChu/TrangChu20.jpg
-    //                         alt="img"
-    //                         width="270px"
-    //                         height="270px"
-    //                       />
-    //                     </div>
-    //                   </a>
-
-    //                   <div className=" absolute hidden top-7/20 left-27/100 group-hover:block">
-    //                     <a href="#">
-    //                       <i className="mt-0 mb-0 mr-[10px] ml-[10px] bg-[#de8ebe] w-[40px] h-[40px] text-white text-[18px] inline-flex justify-center
-    //                       items-center rounded-[100px] fa-regular fa-heart hover:text-[#de8ebe] hover:bg-white group/b1">
-    //                         <div className="absolute bottom-[50px] left-1/2 bg-[#de8ebe] text-white text-[12px] font-[Arial] font-[200] whitespace-nowrap pt-[5px] pb-[5px] pr-[10px] pl-[10px] rounded-[5px] transform hidden -translate-x-1/2 z-100 group-hover/b1:block">Yêu Thích</div>
-    //                       </i>
-    //                     </a>
-    //                     <a href="#">
-    //                       <i className="mt-0 mb-0 mr-[10px] ml-[10px] bg-[#de8ebe] w-[40px] h-[40px] text-white text-[18px] inline-flex justify-center items-center rounded-[100px] hover:text-[#de8ebe] hover:bg-white group/b1 fa-solid fa-magnifying-glass">
-    //                         <div className="absolute bottom-[50px] left-1/2 bg-[#de8ebe] text-white text-[12px] font-[Arial] font-[200] font-weight whitespace-nowrap pt-[5px] pb-[5px] pr-[10px] pl-[10px] rounded-[5px] hidden transform -translate-x-1/2 z-100 group-hover/b1:block">Xem Nhanh</div>
-    //                       </i>
-    //                     </a>
-    //                   </div>
-
-    //                   <div className="function-buy group-hover:inline-block">
-    //                     <div className="function-buy-boder">
-    //                       <button onclick="buy(this)">
-    //                         <span>Mua Hàng</span>
-    //                       </button>
-    //                     </div>
-    //                   </div>
-    //                 </div>
-    //               </div>
-    //               <div className="product-info">
-    //                 <div className="product-info-id">
-    //                   <span>ID:{prodID}</span>
-    //                   <div className="product-info-rate">
-    //                     <i className="fa-regular fa-star"></i>
-    //                     <i className="fa-regular fa-star"></i>
-    //                     <i className="fa-regular fa-star"></i>
-    //                     <i className="fa-regular fa-star"></i>
-    //                     <i className="fa-regular fa-star"></i>
-    //                   </div>
-    //                 </div>
-    //                 <h3>
-    //                   <a href="#">{prodName}</a>
-    //                 </h3>
-    //                 <p>{price}₫</p>
-    //               </div>
-    //             </div>
-    //           </div>
   )
 }
 
